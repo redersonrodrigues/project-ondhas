4d465e66378285969657306777dec3ad7b050786
redersonrodrigues <r.ataliba@hotmail.com>
2022-10-25 13:38:52 -0300

    created validar email único

--- adm/app/adms/Models/AdmsNewUser.php
+++ adm/app/adms/Models/AdmsNewUser.php
@@ -4,40 +4,87 @@ namespace App\adms\Models;
 
 class AdmsNewUser
 {
+    /** @var array|null $data Recebe as informações do formulário */
     private array|null $data;
-    private $result;
 
-    function getResult()
+    /** @var bool $result Recebe true quando executar o processo com sucesso e false quando houver erro */
+    private bool $result;
+
+    /**
+     * @return bool Retorna true quando executar o processo com sucesso e false quando houver erro
+     */
+    function getResult(): bool
     {
         return $this->result;
     }
 
+    /** 
+     * Recebe os valores do formulário.
+     * Instancia o helper "AdmsValEmptyField" para verificar se todos os campos estão preenchidos 
+     * Verifica se todos os campos estão preenchidos e instancia o método "valInput" para validar os dados dos campos
+     * Retorna FALSE quando algum campo está vazio
+     * 
+     * @param array $data Recebe as informações do formulário
+     * 
+     * @return void
+     */
     public function create(array $data = null)
     {
         $this->data = $data;
-        //var_dump($this->data);
 
         $valEmptyField = new \App\adms\Models\helper\AdmsValEmptyField();
         $valEmptyField->valField($this->data);
         if ($valEmptyField->getResult()) {
-
-            $this->data['password'] = password_hash($this->data['password'], PASSWORD_DEFAULT);
-            $this->data['user'] = $this->data['email'];
-            $this->data['created'] = date("Y-m-d H:i:s");
-
-            //var_dump($this->data);
-
-            $createUser = new \App\adms\Models\helper\AdmsCreate();
-            $createUser->exeCreate("adms_users", $this->data);
-
-            if($createUser->getResult()){
-                $_SESSION['msg'] = "<p style='color: green;'>Usuário cadastrado com sucesso!</p>";
-                $this->result = true;
-            }else{
-                $_SESSION['msg'] = "<p style='color: #f00;'>Erro: Usuário não cadastrado com sucesso!</p>";
-                $this->result = false;
-            }            
+            $this->valInput();
         } else {
+            $this->result = false;
+        }
+    }
+
+    /** 
+     * Instanciar o helper "AdmsValEmail" para verificar se o e-mail válido
+     * Instanciar o helper "AdmsValEmailSingle" para verificar se o e-mail não está cadastrado no banco de dados, não permitido cadastro com e-mail duplicado
+     * Instanciar o método "add" quando não houver nenhum erro de preenchimento 
+     * Retorna FALSE quando houve algum erro
+     * 
+     * @return void
+     */
+    private function valInput(): void
+    {
+        $valEmail = new \App\adms\Models\helper\AdmsValEmail();
+        $valEmail->validateEmail($this->data['email']);
+
+        $valEmailSingle = new \App\adms\Models\helper\AdmsValEmailSingle();
+        $valEmailSingle->validateEmailSingle($this->data['email']);
+
+        if (($valEmail->getResult()) and ($valEmailSingle->getResult())) {
+            $this->add();
+        } else {
+            $this->result = false;
+        }
+    }
+
+    /** 
+     * Cadastrar usuário no banco de dados
+     * Retorna TRUE quando cadastrar o usuário com sucesso
+     * Retorna FALSE quando não cadastrar o usuário
+     * 
+     * @return void
+     */
+    private function add(): void
+    {
+        $this->data['password'] = password_hash($this->data['password'], PASSWORD_DEFAULT);
+        $this->data['user'] = $this->data['email'];
+        $this->data['created'] = date("Y-m-d H:i:s");
+
+        $createUser = new \App\adms\Models\helper\AdmsCreate();
+        $createUser->exeCreate("adms_users", $this->data);
+
+        if ($createUser->getResult()) {
+            $_SESSION['msg'] = "<p style='color: green;'>Usuário cadastrado com sucesso!</p>";
+            $this->result = true;
+        } else {
+            $_SESSION['msg'] = "<p style='color: #f00;'>Erro: Usuário não cadastrado com sucesso!</p>";
             $this->result = false;
         }
     }
---
+++ adm/app/adms/Models/helper/AdmsValEmail.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\adms\Models\helper;
+
+/**
+ * Classe genérica para validar o e-mail
+ *
+ * @author Réderson / rederson@ramartecnologia.com.br
+ */
+class AdmsValEmail
+{
+    /** @var string $email Recebe o e-mail que deve ser validado */
+    private string $email;
+
+    /** @var bool $result Recebe true quando executar o processo com sucesso e false quando houver erro */
+    private bool $result;
+
+    /**
+     * @return bool Retorna true quando executar o processo com sucesso e false quando houver erro
+     */
+    function getResult(): bool
+    {
+        return $this->result;
+    }
+
+    /** 
+     * Validar o e-mail.
+     * Recebe o e-mail que deve ser validado e válida o e-mail.
+     * Retorna TRUE quando o e-mail é válido.
+     * Retorna FALSE quando o e-mail é inválido.
+     * 
+     * @param string $email Recebe o e-mail que deve ser validado.
+     * 
+     * @return void
+     */
+    public function validateEmail(string $email): void
+    {
+        $this->email = $email;
+        if(filter_var($this->email, FILTER_VALIDATE_EMAIL)){
+            $this->result = true;
+        }else{
+            $_SESSION['msg'] = "<p style='color: #f00;'>Erro: E-mail inválido!</p>";
+            $this->result = false;
+        }
+    }
+}
