5314e1106141e432e0cf6f89891da699d9400e33
redersonrodrigues <r.ataliba@hotmail.com>
2022-10-31 15:43:15 -0300

    delete img user and directory

--- adm/app/adms/Models/AdmsDeleteUsers.php
+++ adm/app/adms/Models/AdmsDeleteUsers.php
@@ -16,6 +16,15 @@ class AdmsDeleteUsers
     /** @var int|string|null $id Recebe o id do registro */
     private int|string|null $id;
 
+    /** @var array|null $resultBd Recebe os registros do banco de dados */
+    private array|null $resultBd;
+
+    /** @var string $delDirectory Recebe o endereço para apagar o diretório */
+    private string $delDirectory;
+
+    /** @var string $delImg Recebe o endereço para apagar a imagem */
+    private string $delImg;
+
     /**
      * @return bool Retorna true quando executar o processo com sucesso e false quando houver erro
      */
@@ -27,15 +36,59 @@ class AdmsDeleteUsers
     public function deleteUser(int $id): void
     {
         $this->id = (int) $id;
-        $deleteUser = new \App\adms\Models\helper\AdmsDelete();
-        $deleteUser->exeDelete("adms_users", "WHERE id =:id", "id={$this->id}");
 
-        if ($deleteUser->getResult()) {
-            $_SESSION['msg'] = "<p style='color: green;'>Usuário apagado com sucesso!</p>";
-            $this->result = true;
-        } else {
-            $_SESSION['msg'] = "<p style='color: #f00;'>Erro: Usuário não apagado com sucesso!</p>";
+        if($this->viewUser()){
+            $deleteUser = new \App\adms\Models\helper\AdmsDelete();
+            $deleteUser->exeDelete("adms_users", "WHERE id =:id", "id={$this->id}");
+    
+            if ($deleteUser->getResult()) {
+                $this->deleteImg();
+                $_SESSION['msg'] = "<p style='color: green;'>Usuário apagado com sucesso!</p>";
+                $this->result = true;
+            } else {
+                $_SESSION['msg'] = "<p style='color: #f00;'>Erro: Usuário não apagado com sucesso!</p>";
+                $this->result = false;
+            }
+        }else{
             $this->result = false;
+        }
+        
+    }
+
+    private function viewUser(): bool
+    {
+
+        $viewUser = new \App\adms\Models\helper\AdmsRead();
+        $viewUser->fullRead(
+            "SELECT id, image
+                            FROM adms_users                           
+                            WHERE id=:id
+                            LIMIT :limit",
+            "id={$this->id}&limit=1"
+        );
+
+        $this->resultBd = $viewUser->getResult();
+        if ($this->resultBd) {
+            return true;
+        } else {
+            $_SESSION['msg'] = "<p style='color: #f00'>Erro: Usuário não encontrado!</p>";
+            return false;
+        }
+    }
+
+    private function deleteImg(): void
+    {
+        if((!empty($this->resultBd[0]['image'])) or ($this->resultBd[0]['image'] != null)){
+            $this->delDirectory = "app/adms/assets/image/users/" . $this->resultBd[0]['id'];
+            $this->delImg = $this->delDirectory . "/" . $this->resultBd[0]['image'];
+
+            if(file_exists($this->delImg)){
+                unlink($this->delImg);
+            }
+
+            if(file_exists($this->delDirectory)){
+                rmdir($this->delDirectory);
+            }
         }
     }
 }
